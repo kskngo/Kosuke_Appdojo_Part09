66bd1080f43024f130db2f373b426dcbe7f549a8
Kosuke Nagao <kosukengo@gmail.com>
2021-02-23 12:26:19 +0900

    仕様通り動くようになった

---
+++ Kosuke_Appdojo_Part09/InputViewController.swift
@@ -0,0 +1,23 @@
+//
+//  InputViewController.swift
+//  Kosuke_Appdojo_Part09
+//
+//  Created by Kosuke Nagao on 2021/02/22.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    // 選択されたボタンの県名を保持する
+    var prefectureName: String?
+
+    // セグエが実行される前に呼ばれるメソッド
+    // 引数senderはセグエを開始したオブジェクト
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // senderをUIButtonにキャストできた場合（キャンセルボタンはできない）
+        if let selectedButton = sender as? UIButton {
+            prefectureName = selectedButton.titleLabel!.text!
+        }
+    }
+}
--- Kosuke_Appdojo_Part09/ViewController.swift
+++ Kosuke_Appdojo_Part09/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private weak var label: UILabel!
+
+    @IBAction func exit(segue: UIStoryboardSegue) {
+
+        // 遷移先ViewControllerを取得
+        let inputViewController = segue.source as! InputViewController
+
+        //　遷移先ViewControllerの県名が設定されている場合のみラベルを更新する
+        if let prefectureName = inputViewController.prefectureName {
+            label.text = prefectureName
+        }
     }
-
-
 }
 
